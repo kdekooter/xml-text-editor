5bc47cef2869dd3d9d3f70476c69568c36a87ca0
kdekooter <kdekooter@1f02dc9c-b654-0410-aa03-e30831290a1a>
2009-06-05 09:01:04 +0000

    Added smart indenting

--- xml-text-editor/src/main/java/net/boplicity/xmleditor/XmlTextPane.java
+++ xml-text-editor/src/main/java/net/boplicity/xmleditor/XmlTextPane.java
@@ -15,11 +15,20 @@
  */
 package net.boplicity.xmleditor;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+
+import net.boplicity.xmleditor.XmlEditorKit;
 
 
 /**
- * JTextPane implementation that can handle xml text.
+ * JTextPane implementation that can handle xml text. The IndentKeyListener
+ * implements smart indenting.
  *  
  * @author kees
  * @date 27-jan-2006
@@ -28,12 +37,91 @@ import javax.swing.JTextPane;
 public class XmlTextPane extends JTextPane {
 
     private static final long serialVersionUID = 6270183148379328084L;
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     public XmlTextPane() {
         
         // Set editor kit
         this.setEditorKitForContentType("text/xml", new XmlEditorKit());
         this.setContentType("text/xml");
+        
+        addKeyListener(new IndentKeyListener());
     }
+    
+	private class IndentKeyListener implements KeyListener {
+
+		private boolean enterFlag;
+		private final Character NEW_LINE = '\n';
+
+		public void keyPressed(KeyEvent event) {
+			enterFlag = false;
+			if ((event.getKeyCode() == KeyEvent.VK_ENTER)
+					&& (event.getModifiers() == 0)) {
+				if (getSelectionStart() == getSelectionEnd()) {
+					enterFlag = true;
+					event.consume();
+				}
+			}
+		}
+
+		public void keyReleased(KeyEvent event) {
+			if ((event.getKeyCode() == KeyEvent.VK_ENTER)
+					&& (event.getModifiers() == 0)) {
+				if (enterFlag) {
+					event.consume();
+
+					int start, end;
+					String text = getText();
+
+					int caretPosition = getCaretPosition();
+					try {
+						if (text.charAt(caretPosition) == NEW_LINE) {
+							caretPosition--;
+						}
+					} catch (IndexOutOfBoundsException e) {
+					}
+
+					start = text.lastIndexOf(NEW_LINE, caretPosition) + 1;
+					end = start;
+					try {
+						if (text.charAt(start) != NEW_LINE) {
+							while ((end < text.length())
+									&& (Character
+											.isWhitespace(text.charAt(end)))
+									&& (text.charAt(end) != NEW_LINE)) {
+								end++;
+							}
+							if (end > start) {
+								getDocument()
+										.insertString(
+												getCaretPosition(),
+												NEW_LINE
+														+ text.substring(start,
+																end), null);
+							} else {
+								getDocument().insertString(getCaretPosition(),
+										NEW_LINE.toString(), null);
+							}
+						} else {
+							getDocument().insertString(getCaretPosition(),
+									NEW_LINE.toString(), null);
+						}
+					} catch (IndexOutOfBoundsException e) {
+						try {
+							getDocument().insertString(getCaretPosition(),
+									NEW_LINE.toString(), null);
+						} catch (BadLocationException e1) {
+							logger.log(Level.WARNING, e1.toString());
+						}
+					} catch (BadLocationException e) {
+						logger.log(Level.WARNING, e.toString());
+					}
+				}
+			}
+		}
+
+		public void keyTyped(KeyEvent e) {
+		}
+	}
     
 }
